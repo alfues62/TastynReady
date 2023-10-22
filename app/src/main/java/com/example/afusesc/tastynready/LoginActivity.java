4c8d2dedb91394f5133cadb5bd852e90e6711d40
alfues62 <94858416+alfues62@users.noreply.github.com>
2023-10-22 18:21:32 +0200

    AUTENTIFICACION
    funciona

--- app/src/main/java/com/example/afusesc/tastynready/LoginActivity.java
+++ app/src/main/java/com/example/afusesc/tastynready/LoginActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
@@ -14,7 +16,7 @@ import com.google.firebase.auth.FirebaseUser;
 import java.util.Arrays;
 import java.util.List;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends AppCompatActivity {
 
     private static final int RC_SIGN_IN = 123;
     @Override protected void onCreate(Bundle savedInstanceState) {
